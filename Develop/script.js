e87163e52dc26eaf96713435d340a8b2eb0404da
Matthew Pryor <matthewapryor@gmail.com>
2022-03-13 19:32:30 -0400

    structuring password options conditional logic

--- Develop/script.js
+++ Develop/script.js
@@ -1,34 +1,42 @@
 // Assignment code here
-
+var specialChars = "!@#$%^&*()";
+var nums = "0123456789";
+var alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+var passwordLength = 12;
+var password = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Generate password function
 function generatePassword() {
-  var customPass = window.confirm("Our password generator has several customization options. If you would like to add capital letters, numbers or special characters to your password, please press 'OK'. \n\n If you do not want to create a custom password, select 'Cancel' and your basic password will be generated automatically.")
-  if (!customPass) {
+  // PASSWORD PROMPTS ------------------------------------
+  var customPass = window.confirm("Our password generator has several customization options. If you would like to add capital letters, numbers or special characters to your password, please press 'OK'.\n\nIf you do not want to create a custom password, select 'Cancel' and your basic password will be generated automatically.")
+    
+  if (customPass) {
+    if (customPass && withCapitals) {
+      var withCapitals = window.confirm("Would you like your password to contain capitalized letters?")
+      console.log("User has chosen to customize their password with CAPITAL LETTERS.")
+    }
+    else if (customPass && withCapitals && withNumbers) {
+      var withNumbers = window.confirm("Would you like your password to contain numbers?")
+      console.log("User has chosen to customize their password with CAPITAL LETTERS and NUMBERS.")
+    }
+    else if (customPass && withCapitals && withNumbers && withSpecialChars) {
+      console.log("User has chosen to customize their password with CAPITAL LETTERS and NUMBERS and SPECIAL CHARACTERS.")
+      var withSpecialChars = window.confirm("Would you like your password to special characters?")
+    }
+  }
+  else{
     console.log("User has chosen a basic password.")
     // generate password with only lower-case alphabetical chars
   }
-  else if (customPass) {
-    console.log("User has chosen to customize their password.")
-  }
-  else {
-    return
-  }
-  var withCapitals = window.confirm("Would you like your password to contain capitalized letters?")
-  var withNumbers = window.confirm("Would you like your password to contain numbers?")
-  var withSpecialChars = window.confirm("Would you like your password to special characters?")
-  // window.prompt("Would you like your password to contain numbers? If ")
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = 12;
-  var password = "";
-
-  for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber +1);
-   }
+  
+  // // loop through alphabetUpper
+  // for (var i = 0; i <= passwordLength; i++) {
+  //   var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //   var randomNumber = Math.floor(Math.random() * chars.length);
+  //  }
 }
 
 // Write password to the #password input
