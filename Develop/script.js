0acf7ce0eb77e22ee2b8afa46ad8e6888dc38b81
Matthew Pryor <matthewapryor@gmail.com>
2022-03-13 22:32:07 -0400

    structuring criteria logic

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ var passwordLength = Number;
 var password = "";
 let chars;
 let passwordTypeAnswer;
-
+var enteredCriteria = false
 
 
 // Get references to the #generate element
@@ -18,38 +18,92 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   // passwordTypeAnswer = prompt("To make your password more secure, you will need to choose at least 1 criteria from the following:\n\n1 - Capital letters\n2 - Numbers\n3 - Special characters\n\nEnter the number with the corresponding criteria below and confirm your choice.");
   // alert("Passwords must contain between 8 and 128 characters.\n\nHow many characters long would you like your password to be?")
+  while (enteredCriteria = false) {
+    var withCapitals = confirm("Would you like your password to contain capitalized letters?\n\nEx. ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    var withNumbers = confirm("Would you like your password to contain numbers?\n\nEx. 0123456789")
+    var withSpecialChars = confirm("Would you like your password to contain special characters?\n\nEx. !#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+    var criteriaPrompt = "";
 
-  var withCapitals = confirm("Would you like your password to contain capitalized letters?\n\nABCDEFGHIJKLMNOPQRSTUVWXYZ")
-  var withNumbers = confirm("Would you like your password to contain numbers?\n\n0123456789")
-  var withSpecialChars = confirm("Would you like your password to contain special characters?\n\n!#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
-  var criteriaPrompt = "";
-
-  if (withCapitals && withNumbers && withSpecialChars) {
-    criteriaPrompt = "Your password will contain capital letters AND numbers AND special characters."
-    console.log(criteriaPrompt)
-    return criteriaPrompt
+    if (withCapitals) {
+      if (!withNumbers && withSpecialChars) {
+        criteriaPrompt = "Your password will contain:\nCapital letters ✅\nSpecial characters ✅"
+        console.log(criteriaPrompt)
+        enteredCriteria = true
+        return criteriaPrompt
+      }
+      else if (withNumbers && !withSpecialChars) {
+        criteriaPrompt = "Your password will contain:\nCapital letters ✅\nNumbers ✅"
+        console.log(criteriaPrompt)
+        enteredCriteria = true
+        return criteriaPrompt
+      } 
+      else if (withNumbers && withSpecialChars) {
+        criteriaPrompt = "Your password will contain:\nCapital letters ✅\nNumbers ✅\nSpecial characters ✅"
+        console.log(criteriaPrompt)
+        enteredCriteria = true
+        return criteriaPrompt
+      } 
+      else if (!withNumbers && !withSpecialChars) {
+        criteriaPrompt = "Your password will contain:\nCapital letters ✅"
+        console.log(criteriaPrompt)
+        enteredCriteria = true
+        return criteriaPrompt
+      } 
+      else {
+        return "This is the else statement returning."
+      }
+    }
+    // if no capital letters
+    else if (withNumbers) {
+      if (!withCapitals && withSpecialChars) {
+        criteriaPrompt = "Your password will contain:\nNumbers ✅\nSpecial characters ✅"
+        console.log(criteriaPrompt)
+        enteredCriteria = true
+        return criteriaPrompt
+      }
+      else if (!withCapitals && !withSpecialChars) {
+        criteriaPrompt = "Your password will contain:\nNumbers ✅"
+        console.log(criteriaPrompt)
+        enteredCriteria = true
+        return criteriaPrompt
+      } 
+      else {
+        return "This is the else statement returning."
+      }
+    }
+    // if no capital letters and no numbers
+    else if (withSpecialChars) {
+      criteriaPrompt = "Your password will contain:\nSpecial characters ✅"
+      console.log(criteriaPrompt)
+      enteredCriteria = true
+      return criteriaPrompt
+    }
+    // if user did not select at least one criteria
+    else {
+      return "To make your password more secure, please select at least 1 of the required criteria."
+    }
   }
-  if (!withCapitals && withNumbers && withSpecialChars) {
-    criteriaPrompt = "Your password will contain numbers AND special characters."
-    console.log(criteriaPrompt)
-    return criteriaPrompt
-  }
-  if (!withCapitals && !withNumbers && withSpecialChars) {
-    criteriaPrompt = "Your password will contain special characters."
-    console.log(criteriaPrompt)
-    return criteriaPrompt
-  }
-  if (!withCapitals && withNumbers && !withSpecialChars) {
-    criteriaPrompt = "Your password will contain capital letters AND special characters."
-    console.log(criteriaPrompt)
-    return criteriaPrompt
-  }
-  if (withCapitals && !withNumbers && !withSpecialChars) {
-    criteriaPrompt = "Your password will contain numbers AND special characters."
-    console.log(criteriaPrompt)
-    return criteriaPrompt
-  }
-  
+  // else if (withNumbers) {
+  //   if (!withCapitals && withSpecialChars) {
+  //     criteriaPrompt = "Your password will contain:\nNumbers ✅\nSpecial characters ✅"
+  //     console.log(criteriaPrompt)
+  //     return criteriaPrompt
+  //   } 
+  //   else {
+  //     return "This is the else statement returning."
+  //   }
+  // }
+  // else if (withSpecialChars) {
+  //   if (!withCapitals && !withNumbers) {
+  //     criteriaPrompt = "Your password will contain:\nSpecial characters ✅"
+  //     console.log(criteriaPrompt)
+  //     return criteriaPrompt
+  //   }
+  // }
+  // else {
+  //   return "This is the else statement returning."
+  // }
+}
   
 
   
@@ -75,7 +129,7 @@ function generatePassword() {
   //   var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   //   var randomNumber = Math.floor(Math.random() * chars.length);
   //  }
-}
+
 
 // Write password to the #password input
 function writePassword() {
