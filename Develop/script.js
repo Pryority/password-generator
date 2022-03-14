bfed3306bf4a12925046bc21f881c2f215ed3a41
Matthew Pryor <matthewapryor@gmail.com>
2022-03-14 14:31:39 -0400

    redid password criteria logic

--- Develop/script.js
+++ Develop/script.js
@@ -8,35 +8,98 @@
 // 2. Validate the input
 // 3. Generate password based on criteria
 
-function generatePassword() {
-  var passwordLength = prompt("How many characters would you like your password to contain?");
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("Passwords must contain between 8 and 128 characters. Please try again.");
-  }
-  else if (passwordLength > 8 && passwordLength < 128) {
-    console.log("Password length: ", passwordLength)
-    
-    var withSpecialChars = confirm("Click OK to include special characters in your password");
-    console.log(withSpecialChars)
-    var withLower = confirm("Click OK to include lower-case letters in your password");
-    console.log(withLower)
-    var withUpper = confirm("Click OK to include upper-case letters in your password");
-    console.log(withUpper)
-    var withNumbers = confirm("Click OK to include numbers in your password");
-    console.log(withNumbers)
-  }
-}
-
+const alpha = "abcdefghijklmnopqrstuvwxyz";
+const alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*_-+=";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength = prompt("How many characters would you like your password to contain?");
+var withUpper = confirm("Would you like your password to contain upper-case letters?\n\nEx. ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var withNumbers = confirm("Would you like your password to contain numbers?\n\nEx. 0123456789")
+var withSymbols = confirm("Would you like your password to contain symbols?\n\nEx. !@#$%^&*_-+=")
+
+
+function generatePassword (length, characters) {
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    // all criteria
+    if (withUpper && withNumbers && withSymbols) {
+      let characters = alpha + alphaUpper + numbers + symbols
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+      return password;
+
+    }
+    // no numbers
+    else if (withUpper && !withNumbers && withSymbols) {
+      let characters = alpha + alphaUpper + symbols
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+      return password;
+
+    }
+    // no upper-case
+    else if (!withUpper && withNumbers && withSymbols) {
+      let characters = alpha + numbers + symbols
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+      return password;
+
+    }
+    // no upper-case, numbers
+    else if (!withUpper && !withNumbers && withSymbols) {
+      let characters = alpha + symbols
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+      return password;
+
+    }
+      // no upper-case, symbols
+    else if (!withUpper && withNumbers && !withSymbols) {
+      let characters = alpha + numbers
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+      return password;
+
+    }
+    // no symbols
+    else if (withUpper && withNumbers && !withSymbols) {
+      let characters = alpha + alphaUpper + numbers
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+      return password;
+
+    }
+      // no numbers, symbols
+    else if (withUpper && !withNumbers && !withSymbols) {
+      let characters = alpha + numbers
+      password += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+      return password;
+
+    }
+  
+  }
+  return password;
+  
+};
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  // var characters = alpha
 
-  passwordText.value = password;
+  passwordText.value = generatePassword();
 
 }
 
