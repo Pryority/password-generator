92789f4594ff4cddde7326568b832168896d8300
Matthew Pryor <matthewapryor@gmail.com>
2022-03-14 00:17:22 -0400

    yup i think i messed all of my code up

--- Develop/script_old.js
+++ Develop/script_old.js
@@ -24,54 +24,48 @@ switch(chars) {
     characters = 'abcdefghijklmnopqrstuvwxyz';
 }
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Generate password function
 function generatePassword(length) {
+  let result = ' ';
+  const charactersLength = characters.length;
   var withCapitals = confirm("Would you like your password to contain capitalized letters?\n\nEx. ABCDEFGHIJKLMNOPQRSTUVWXYZ")
   var withNumbers = confirm("Would you like your password to contain numbers?\n\nEx. 0123456789")
   var withSpecialChars = confirm("Would you like your password to contain special characters?\n\nEx. !#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
   var criteriaPrompt = "";
   var enteredCriteria = false
-
-  if (withCapitals) {
-    if (!withNumbers && withSpecialChars) {
-      criteriaPrompt = "Your password contains:\nCapital letters ✅\nSpecial characters ✅"
-      console.log(criteriaPrompt)
-      enteredCriteria = true
-      console.log(chars)
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  for ( let i = 0; i < length; i++ ) {
+    if (withCapitals) {
+      if (!withNumbers && withSpecialChars) {
+        criteriaPrompt = "Your password contains:\nCapital letters ✅\nSpecial characters ✅"
+        console.log(criteriaPrompt)
+        enteredCriteria = true
+        console.log(chars)
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      else if (withNumbers && !withSpecialChars) {
+        criteriaPrompt = "Your password contains:\nCapital letters ✅\nNumbers ✅"
+        console.log(criteriaPrompt)
+        enteredCriteria = true
+        
+      } 
+      else if (withNumbers && withSpecialChars) {
+        criteriaPrompt = "Your password contains:\nCapital letters ✅\nNumbers ✅\nSpecial characters ✅"
+        console.log(criteriaPrompt)
+        enteredCriteria = true
+        
+      } 
+      else if (!withNumbers && !withSpecialChars) {
+        criteriaPrompt = "Your password contains:\nCapital letters ✅"
+        console.log(criteriaPrompt)
+        enteredCriteria = true
+        
+      }
+      else {
+        return "This is the else statement returning."
+      }
     }
-    else if (withNumbers && !withSpecialChars) {
-      criteriaPrompt = "Your password contains:\nCapital letters ✅\nNumbers ✅"
-      console.log(criteriaPrompt)
-      enteredCriteria = true
-      console.log(chars)
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      
-    } 
-    else if (withNumbers && withSpecialChars) {
-      criteriaPrompt = "Your password contains:\nCapital letters ✅\nNumbers ✅\nSpecial characters ✅"
-      console.log(criteriaPrompt)
-      enteredCriteria = true
-      console.log(chars)
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      
-    } 
-    else if (!withNumbers && !withSpecialChars) {
-      criteriaPrompt = "Your password contains:\nCapital letters ✅"
-      console.log(criteriaPrompt)
-      enteredCriteria = true
-      console.log(chars)
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      
-    }
-    else {
-      return "This is the else statement returning."
-    }
-  }
   // if no capital letters
   else if (withNumbers) {
     if (!withCapitals && withSpecialChars) {
@@ -101,13 +95,19 @@ function generatePassword(length) {
   else {
     return "To make your password more secure, please select at least 1 of the required criteria."
   }
+  }
+
+  return result;
+
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
